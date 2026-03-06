93b91d5f3cc9551b683bc261a18c2822f287024e
Joshua <joshua.quiroga-l@mail.escuelaing.edu.co>
2026-03-05 21:49:23 -0500

    [feature]: Creacion de la estructura basica del proyecto

---
+++ src/main/java/edu/dosw/proyecto/Tech_Cup_Football_2026_1/TechCupFootball20261Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.proyecto.Tech_Cup_Football_2026_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCupFootball20261Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCupFootball20261Application.class, args);
+	}
+
+}
